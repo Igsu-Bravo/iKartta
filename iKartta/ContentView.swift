286749641b07ca0244b813d467fffd016119451f
Igsu <juan.bravo@futurice.com>
2022-06-21 19:23:00 +0300

    Initial Commit

---
+++ iKartta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iKartta
+//
+//  Created by Juan Bravo on 21.6.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iKartta/iKarttaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKarttaApp.swift
+//  iKartta
+//
+//  Created by Juan Bravo on 21.6.2022.
+//
+
+import SwiftUI
+
+@main
+struct iKarttaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
